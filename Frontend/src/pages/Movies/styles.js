18129404c1561dfe76037e30183cc1d41bdb040a
Euclécio Josias Rodrigues <eucjosias@gmail.com>
2019-09-22 22:32:59 -0300

    Create input to search movies

--- Frontend/src/pages/Movies/styles.js
+++ Frontend/src/pages/Movies/styles.js
@@ -7,3 +7,16 @@ export const Container = styled.div`
   flex-wrap: wrap;
   justify-content: space-evenly;
 `
+
+export const Search = styled.input`
+	border: none;
+	border-bottom: 2px solid #55555a;
+	background-color: transparent;
+	width: 500px;
+	margin: 0 auto;
+	padding: 10px;
+	outline: none;
+	color: #77777a;
+	font-size: 20px;
+	display: block;
+`
